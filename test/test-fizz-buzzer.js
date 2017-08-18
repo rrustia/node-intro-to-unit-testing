32ca4ba730adefc90190745f7fadb8c77792794c
Ronel Rustia <fedstats@yahoo.com>
2017-08-18 12:15:22 -0700

    Implemented mochai tests for fizz

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -3,20 +3,20 @@ const should = require('chai').should();
 const fizzbuzz = require('../fizzBuzzer');
 
 
-// unit tests for our `adder` function
+// unit tests for our `fizzbuzz` function
 describe('fizzbuzz', function() {
 
   // test the normal case
   it('should correctly evaluate a number according to fizzbuzz rules', function() {
     // range of normal inputs, including
-    // notable cases like negative answers
+    // notable cases like non-fizz-buzz numbers
     const normalCases = [
       {a: 15, expected: 'fizz-buzz'},
       {a: 5, expected: 'buzz'},
       {a: 3, expected: 'fizz'},
       {a: 2, expected: 2}
     ];
-    // for each set of inputs (a, b), `adder` should
+    // for each set of inputs (a), `fizzbuzz` should
     // produce the expected value
     normalCases.forEach(function(input) {
       const answer = fizzbuzz(input.a);
@@ -24,13 +24,13 @@ describe('fizzbuzz', function() {
     });
   });
 
-  it('should raise error if args not numbers', function() {
-    // range of bad inputs where not both are numbers
+  it('should raise error if arg is not a number', function() {
+    // range of bad inputs are not numbers
     const badInputs = ['a', 'w', 'test'];
     // prove that an error is raised for bad inputs
     badInputs.forEach(function(input) {
       (function() {
-          adder(input)
+          fizzbuzz(input)
       }).should.throw(Error);
     });
   });
